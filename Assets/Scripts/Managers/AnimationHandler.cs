49a9a58df855fd74dd08b8131cec3189ee07822c
Tabee <ayvanika@gmail.com>
2024-11-18 14:47:43 +0200

    floating item & animation tweaks

--- Assets/Scripts/Managers/AnimationHandler.cs
+++ Assets/Scripts/Managers/AnimationHandler.cs
@@ -6,10 +6,14 @@ using UnityEngine.UI;
 public class AnimationHandler : MonoBehaviour
 {
     public static AnimationHandler instance;
-    [SerializeField] private Animator floatingAnimator;
+    [SerializeField] private Animator checkAnimator;
+    [SerializeField] private Animator foodAnimator;
+    [SerializeField] private Animator itemAnimator;
     [SerializeField] private Animator foodbarAnimator;
-    [SerializeField] private GameObject location;
-    [SerializeField] private Image imageAtCursor;
+    [SerializeField] private GameObject atCursorLocation;
+    [SerializeField] private Image checkAtCursor;
+    [SerializeField] private Image foodAtCursor;
+    [SerializeField] private Image itemAtCursor;
     [SerializeField] private Sprite success;
     [SerializeField] private Sprite failure;
     private void Awake()
@@ -26,33 +30,39 @@ public class AnimationHandler : MonoBehaviour
     }
     public void InteractionFeedback(bool isSuccess)
     {
-        location.transform.position = Input.mousePosition;
+        atCursorLocation.transform.position = Input.mousePosition;
 
         if (isSuccess)
         {
-            imageAtCursor.sprite = success;
-            floatingAnimator.SetTrigger("Success");
+            checkAtCursor.sprite = success;
+            checkAnimator.SetTrigger("Success");
         }
         else
         {
-            imageAtCursor.sprite = failure;
-            floatingAnimator.SetTrigger("Failure");
+            checkAtCursor.sprite = failure;
+            checkAnimator.SetTrigger("Failure");
         }
     }
     public void FoodFeedback(Sprite sprite)
     {
-        floatingAnimator.enabled = false;
-
-        location.transform.position = Input.mousePosition;
-        Debug.Log($"sprite is: {sprite}");
-        
-        imageAtCursor.sprite = sprite;
-        floatingAnimator.enabled = true;
-        floatingAnimator.SetTrigger("ImageTooltip");
+        foodAnimator.enabled = false;
+        atCursorLocation.transform.position = Input.mousePosition;
+        foodAtCursor.sprite = sprite;
+        foodAnimator.enabled = true;
+        foodAnimator.SetTrigger("ImageTooltip");
     }
 
     public void ShowFoods()
     {
         foodbarAnimator.SetTrigger("ShowFoods");
+    }
+
+    public void ItemFeedback(Sprite sprite)
+    {
+        itemAnimator.enabled = false;
+        atCursorLocation.transform.position = Input.mousePosition;
+        itemAtCursor.sprite = sprite;
+        itemAnimator.enabled = true;
+        itemAnimator.SetTrigger("ItemTooltip");
     }
 }
