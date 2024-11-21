77db2406fedfcd3a4b8ecbad896dae83ad78422d
Tabee <ayvanika@gmail.com>
2024-11-21 12:57:19 +0200

    desert puzzles updated

--- Assets/Scripts/Audio/PlayAudio.cs
+++ Assets/Scripts/Audio/PlayAudio.cs
@@ -2,12 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayAudio : MonoBehaviour
+public class PlayAudio : MonoBehaviour, IResult
 {
 
     public AudioSource soundPlayer;
 
     public void PlayThisSoundEffect()
+    {
+        soundPlayer.Play();
+    }
+    public void Execute()
     {
         soundPlayer.Play();
     }
